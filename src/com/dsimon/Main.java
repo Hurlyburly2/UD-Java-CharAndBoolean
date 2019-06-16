c5618a4d5096f8eb24f2bc966e5b8261307a1bdf
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-16 17:37:48 -0400

    lesson done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,12 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // width 16 (2 bytes)
+	    char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        // holds true or false
+        boolean myBoolean = true;
+        boolean isMale = true;
     }
 }
